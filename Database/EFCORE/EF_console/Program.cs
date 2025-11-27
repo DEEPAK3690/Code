3f434578871bb0ca1082eb5b56fd951c1e282ca0
DEEPAK <101787153+deepak-grl@users.noreply.github.com>
2025-11-27 15:05:33 +0530

    console project

---
+++ Database/EFCORE/EF_console/Program.cs
@@ -0,0 +1,33 @@
+﻿using EF_DataAccess;
+using Microsoft.EntityFrameworkCore;
+namespace EF_console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new ApplicationDbcontext())
+            {
+                context.Database.EnsureCreated();
+
+                if (context.Database.GetPendingMigrations().Count() > 0)// to enusure migration is created
+                {
+                    context.Database.Migrate();
+                }
+
+                getallbooks(context);
+
+            }
+
+        }
+
+        static void getallbooks(ApplicationDbcontext applicationDbcontext)
+        {
+            var books = applicationDbcontext.Books.ToList();
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Book Id: {book.BookId}, Book Title: {book.Title}");
+            }
+        }
+    }
+}
